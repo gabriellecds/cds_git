c637a6cd7e0849b08e077cf82b16910f797853ac
Gabrielle Oliveira <gabrielle97oliveira@gmail.com>
2023-06-19 15:05:10 -0300

    Resolvido conflito de merge

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def create_answers_section(df):
     st.subheader("Which company has the most expensive bikes on avarege?")
 
     st.subheader("Are the company that has the most expensive bikes registered also the company with the most bikes registered?")
-
+    
     st.subheader("Which bikes are good for buying?")
     
     return None
